b87dcf508a68f7df2f76faf48c29d20ae753a2af
AnnalisaBel <55884820+AnnalisaBel@users.noreply.github.com>
2019-10-03 11:51:10 +0200

    Modificato attività di start
    Aggiunto intent

--- app/src/main/java/com/example/bulletmaster/StartActivity.java
+++ app/src/main/java/com/example/bulletmaster/StartActivity.java
@@ -11,4 +11,12 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+     Button butstartgame = findViewById(R.id.butstartgame);
+        butstartgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intgotolev = new Intent(StartActivity.this, LevelsActivity.class);
+                startActivity(intgotolev);
+            }
+        });
 }
